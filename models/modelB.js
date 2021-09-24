2146d1e0e0e72a8ecb20dff6f4ef0195c563e364
danielsolomon332 <daniel.solomon332@gmail.com>
2021-09-24 16:41:12 -0600

    Initial commit

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {
+  firstName: "Daniel",
+  lastName: "Solomon",
+  height: 6,
+  age: 31.2,
+  isWise: true,
+  isNervous: false,
+  favoriteColors: ["blue", "green", "white"],
+  familyMembers: ["Noah", "Justin", "Suzanne"],
+}

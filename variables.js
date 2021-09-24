31ca67be4df94063536f911062af0a863faaf1da
danielsolomon332 <daniel.solomon332@gmail.com>
2021-09-24 16:23:38 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,17 @@
+var movie = "Gladiator";
+
+var isEntertaining = true;
+
+movie = "Step Brothers";
+
+var num4 = 7;
+
+var num6 = 11;
+
+var product = (num6 * num4);
+
+var favoriteFoods = ["pizza", "chips", "pasta"];
+
+favoriteFoods.shift();
+
+console.log(movie.length);

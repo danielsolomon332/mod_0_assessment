01e9de3a4c5c5a0d9e994e946751511dc36f5201
danielsolomon332 <daniel.solomon332@gmail.com>
2021-09-24 16:36:04 -0600

    Initial commit

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaur = {
+  type: "T-rex",
+  name: "Betty",
+  height: 10,
+  numberOfClaws: 8,
+  isHangry: true,
+  isSmelly: true,
+  diet: ["meat", "fish", "animals"],
+  friends: ["Jim", "Bill", "Sue"],
+}
